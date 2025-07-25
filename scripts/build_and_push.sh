fbd21ba73e5ce79244e55e3e7ae843ab4edaadf2
Raj <Raj>
2025-07-25 03:04:08 +0000

    ok

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -6,7 +6,7 @@ GIT_COMMIT=$(git rev-parse --short HEAD)
 IMAGE="nageshdocker25/myapp:$GIT_COMMIT"
 
 echo "[INFO] Building image: $IMAGE"
-docker build -t "$IMAGE" .
+DOCKER_BUILDKIT=0 docker build -t "$IMAGE" .
 
 echo "[INFO] Logging into Docker Hub..."
 docker login
